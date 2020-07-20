3c5abd24b077ddfad437084206f3f98dda7159f2
Crowdbotics <team@crowdbotics.com>
2020-07-20 18:18:32 +0000

    Update models

--- backend/modules_and_screens__7211/wsgi.py
+++ backend/modules_and_screens__7211/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modules_and_screens__7211.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "modules_and_screens__7211.settings")
 
 application = get_wsgi_application()
